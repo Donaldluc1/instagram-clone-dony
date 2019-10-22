37d9a8d9fc4883f3dacaf3dd63a9d89ad689a77d
Atangana Lucien <luciendonald93@gmail.com>
2019-10-22 13:53:49 +0100

    Donaldluc instagram clone

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to AtanGram
+
+This is a community of follow developers and we love that you have joined us
+
+
+Thanks, and all the best<br>
+Atangana
+@endcomponent
